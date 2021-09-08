6a9e07e778cddfe05cbfe0dd10928557755ab591
John Machado <jrmachado@electro-matic.com>
2021-09-07 20:36:45 -0400

    class is over

--- scratchpad_2021_09_07.py
+++ scratchpad_2021_09_07.py
@@ -1,2 +1,19 @@
-for i in range(99):
-    print('it works')
+from numpy import (
+    matmul, sin, cos
+) 
+
+
+p = [
+    [-1, 2, 5], 
+    [10, 12, 5], 
+    [4, 7, 2]
+]
+t = 3.14/2
+r = [
+    [0, -1, 0], 
+    [1, 0, 0], 
+    [0, 0, 1]
+]
+
+for row in p: print(row)
+print (matmul(p,r))
