63d7f98bc0a2c47d33b901ed4fa62f6be6361411
John Machado <jrmachado@electro-matic.com>
2021-11-15 13:22:12 -0500

    ?

--- scratchpad_2021_09_07.py
+++ scratchpad_2021_09_07.py
@@ -1,19 +1,6 @@
-from numpy import (
-    matmul, sin, cos
-) 
+import pandas as pd
+import numpy as np
 
+s = pd.Series(np.random.randn(4))
 
-p = [
-    [-1, 2, 5], 
-    [10, 12, 5], 
-    [4, 7, 2]
-]
-t = 3.14/2
-r = [
-    [0, -1, 0], 
-    [1, 0, 0], 
-    [0, 0, 1]
-]
-
-for row in p: print(row)
-print (matmul(p,r))
+print (s.ndim)
